1963f0aa77dd8c13a88871bee4f2a106e059e7a9
Muyinda ROgers <moverr@gmail.com>
2020-06-26 02:27:22 +0300

    Note Activity

--- app/src/main/java/com/kodeinc/notekeeper/NoteActivity.java
+++ app/src/main/java/com/kodeinc/notekeeper/NoteActivity.java
@@ -1,15 +1,18 @@
 package com.kodeinc.notekeeper;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
